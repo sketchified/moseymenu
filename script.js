7737729323f09164ab347333f03d33da1fb824c7
sketchified <47341898+sketchified@users.noreply.github.com>
2021-12-03 16:11:23 -0800

    commit

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
-var count = 1;
+const flashcards = document.getElementsByClassName ("flashcards")[0];
+const createBox = document.getElementsByClassName ("create-box")[0];
+const question = document.getElementById ("question")
+const answer = document.getElementById ("answer")
+let contentArray = localStorage.getItem('items') ? JSON.parse(localstorage.getItem('items')): [];
 
-function changeCount(num) {
-    count += num;
-    document.getElementById("count").innerHTML = count;
+contentArray.forEach(divMaker);
+
+function divMaker(text){
+ var div = document.createElement ("div");
+ var h2_question = document.createElement ("h2");
+ var h2_answer = document.createElement ("h2");
+
+    div.className = 'flashcard';
+
+h2_question.setAttribute('style',"border-top:1px solid red; padding: 15px; margin-top:30px");
+h2_question.innerHTML = text.my_question;
+
+h2_answer.setAttribute("style","text-align:center; display: none; color:red");
+h2_answer.innerHTML = text.my_answer;
+
+div.appendChild(h2_question);
+div.appendChild(h2_answer);
+div.addEventListener("click", function()
+{if(h2_answer.style.display =="none") h2_answer.style.display = "blocK";
+else
+h2_answer.style.display = "none";
+}
+);
+
+
+flashcards.appendChild(div);
+
 }
 
+function addFlashcard(){
+    var flashcard_info = {
+        'my_question' : question.value,
+        'my_answer' : answer.value
+    }
+    contentArray.push(flashcard_info);
+    localStorage.setItem('items', JSON.stringify(contentArray));
+    divMaker(content[content.length - 1]);
+    question.value = '';
+    answer .value = '';
+}
 
+function delFlashcards(){
+    localStorage.clear();
+    flashcards.innerHTML = '';
+    contentArray = [];
+}
+
+function showCreateCardBox(){
+    createBox.style.display = 'block';
+    
+}
+
+function hideCreateBox(){
+    createBox.style.display = 'none';
+
+}
